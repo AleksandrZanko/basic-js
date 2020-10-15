ca46a0bf1d12c65f93f59612382d1fa951d3f006
Aleksandr Zanko <58461608+AleksandrZanko@users.noreply.github.com>
2020-10-15 17:26:07 +0300

    feat: add function to weather

--- src/what-season.js
+++ src/what-season.js
@@ -1,13 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+	if(date == undefined) {
+		return 'Unable to determine the time of year!';
+	} 
+	
 	let year = date.getFullYear();
 	let month = date.getMonth();
 	let day = date.getDate();
 	
-	if(date == undefined) {
-		return 'Unable to determine the time of year!';
-	} 
+	
 
 	if (date.getFullYear() == year && date.getMonth() == month && date.getDate() == day) {
 	
