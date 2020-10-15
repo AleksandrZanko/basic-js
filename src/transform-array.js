567f59a4386e0e9448c1afc7c71903ecc2782353
Aleksandr Zanko <58461608+AleksandrZanko@users.noreply.github.com>
2020-10-15 17:00:48 +0300

    faet: add array transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	let result = [];
+
+	for(let i = 0; i < arr.length; i++) {
+
+
+		if((arr[i - 1] !== '--discard-next' && arr[i] !== '--discard-next') && 
+			(arr[i + 1] !== '--discard-prev' && arr[i] !== '--discard-prev') &&
+			arr[i] !== '--double-next' && arr[i] !== '--double-prev' 
+			&& arr[i + 1] !== '--double-prev') {
+
+			result.push(arr[i]);
+
+	} 
+
+	if(arr[i] == '--double-next') {	
+		if(arr[i + 1] != undefined) {			
+			result.push(arr[i + 1]);		
+		}
+	}
+
+	if(arr[i] == '--double-prev') {
+		if(arr[i - 1] != undefined && arr[i - 2] != '--discard-next') {
+			result.push(arr[i - 1]);
+			result.push(arr[i - 1]);
+		}
+	}
+
+}
+return (result);
 };
